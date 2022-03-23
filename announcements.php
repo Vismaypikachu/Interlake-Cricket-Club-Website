a3591e8995262eac17e94d329ff50efb26bad493
Vismay Patel <vismaypikachu@gmail.com>
2022-03-23 04:43:11 +0000

    css

--- announcements.php
+++ announcements.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="announcements.css" type="text/css">
     <title>Interlake Cricket Club | Announcements</title>
     <link rel="icon" type="image/x-icon" href="/src/images/ICC LOGO.svg">
@@ -74,6 +75,14 @@
         <h3 style="font-size: 16px;">Created and Coded by <a style="color: white; text-decoration: underline;" href="https://vismaypatel.com">Vismay Patel</a> © 2022</h3>
     </footer>
     
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script>
+        function jumpScroll(x) {
+            if (x == 1) {
+                window.scroll(0, 2175);
+            }
+        }
+    </script>
 </body>
 
 </html>
