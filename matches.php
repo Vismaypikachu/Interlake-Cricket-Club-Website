f54849895b22c8237f454f4274547f5166099a49
Vismay Patel <vismaypikachu@gmail.com>
2022-03-23 16:34:41 +0000

    css

--- matches.php
+++ matches.php
@@ -37,7 +37,7 @@
 
         <table id="table">
             <tr>
-                <th style="width: 15%;">ID (Internal Use)</th>
+                <th style="width: 10%;">ID (Internal Use)</th>
                 <th>Match Date</th>
                 <th>Home Team</th>
                 <th>Away Team</th>
