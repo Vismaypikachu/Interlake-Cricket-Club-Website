e1e89124325b73b6abb747a0045641515c196695
Vismay Patel <vismaypikachu@gmail.com>
2022-03-24 19:21:59 +0000

    css

--- matches/defaultMatch.php
+++ matches/defaultMatch.php
@@ -64,7 +64,7 @@
         </div>
 
 
-        <div style = "width: 80%; height: 1100px" class = "matchResults">
+        <div style = "width: 80%; height: 1200px; margin: 0 auto;" class = "matchResults">
             <?php echo "<iframe style = \"width: 100%; height: 100%\"class = \"resultsPDF\" src = \"/". $resultsURL ."\"></iframe>"; ?>
         </div>
     </div>  
