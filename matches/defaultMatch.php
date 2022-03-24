ccf50a02d00d628e12649a8292f9cbcadec91955
Vismay Patel <vismaypikachu@gmail.com>
2022-03-24 19:40:21 +0000

    css

--- matches/defaultMatch.php
+++ matches/defaultMatch.php
@@ -68,7 +68,7 @@
 
 
         <div style = "width: 80%; height: 1150px; margin: 0 auto;" class = "matchResults">
-            <?php echo "<iframe style = \"width: 100%; height: 100%\"class = \"resultsPDF\" src = \"/". $resultsURL ."#toolbar=0&navpanes=0&scrollbar=0\"></iframe>"; ?>
+            <?php echo "<iframe style = \"width: 75%; height: 100%\"class = \"resultsPDF\" src = \"/". $resultsURL ."#toolbar=0&navpanes=0&scrollbar=0\"></iframe>"; ?>
         </div>
     </div>  
 
