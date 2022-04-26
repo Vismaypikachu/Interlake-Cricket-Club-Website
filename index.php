55450fcaff0af32e55a06b2b8c05eb0ecead8950
Vismay Patel <vismaypikachu@gmail.com>
2022-04-26 14:51:38 -0700

    navbar update

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
+                <img class="d-inline-block align-text-top" height="60px" src="/src/images/ICC LOGO.svg">
                 Interlake Cricket Club
-                <img height="60px" src="/src/images/ICC LOGO.svg">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
