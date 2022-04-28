af3960b85ccdd91eed23c324041d222244c48dd5
Vismay Patel <vismaypikachu@gmail.com>
2022-04-28 12:41:30 -0700

    css

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://unpkg.com/bootstrap-table@1.20.0/dist/bootstrap-table.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="index.css" rel="stylesheet" type="text/css">
     <title>Interlake Cricket Club | Home</title>
@@ -58,7 +57,7 @@
     <div class = "schedule">
         <h2 id = "eventHeader"><span class = "red">- </span>Upcoming Events<span class = "red"> -</span></h2>
         <div class="table-responsive">
-            <table class = "schedule table table-hover" data-toggle="table" data-mobile-responsive = "true">
+            <table class = "schedule table table-hover">
                 <th>Meeting List</th>
                 <th>Are we meeting?</th>
                 <th>When?</th>
@@ -165,7 +164,6 @@
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/bootstrap-table@1.20.0/dist/bootstrap-table.min.js"></script>
     <script>
         function jumpScroll(x) {
             if(x == 1){
