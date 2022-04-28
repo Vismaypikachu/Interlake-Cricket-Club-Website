6fb6bd7219c2ef1e3edeaa81f4cd11c1326321e9
Vismay Patel <vismaypikachu@gmail.com>
2022-04-27 21:03:34 -0700

    css

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
             </button>
 
             <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav ml-auto">
+                <ul class="nav navbar-nav navbar-right">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
