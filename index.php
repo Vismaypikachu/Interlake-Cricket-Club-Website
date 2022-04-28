ee9b1d5ff0fde48c8aba7d499d94308991e6289a
Vismay Patel <vismaypikachu@gmail.com>
2022-04-27 20:54:38 -0700

    alignment

--- index.php
+++ index.php
@@ -20,8 +20,8 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="navLinks collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
