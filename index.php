692edb27db49c3f869153cd1bf152ddcf8f274d8
Vismay Patel <vismaypikachu@gmail.com>
2022-04-28 14:40:33 -0700

    css

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                         <a class="nav-link" href="javascript:jumpScroll(1)">Join the Club</a>
                     </li>
                     <li class="nav-item" style = "vertical-align: middle;">
-                        <a class="nav-link" href="matchesList.php" style = "display: flex; text-align: center; vertical-align: middle;">Matches<p class="badge bg-secondary">New</p></a>
+                        <a class="nav-link" href="matchesList.php" style = "display: flex; text-align: center; vertical-align: middle;">Matches<span class="badge bg-secondary">New</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="IYANVGO.php">IYANVGO!</a>
