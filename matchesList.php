1d20638859b36566c7bc2ef46880104c4626d8b9
Vismay Patel <vismaypikachu@gmail.com>
2022-04-28 11:22:52 -0700

    matches fix

--- matchesList.php
+++ matchesList.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="matches.css" type="text/css">
+    <link rel="stylesheet" href="matchesList.css" type="text/css">
     <title>Interlake Cricket Club | Matches</title>
     <link rel="icon" type="image/x-icon" href="/src/images/ICC LOGO.svg">
 </head>
