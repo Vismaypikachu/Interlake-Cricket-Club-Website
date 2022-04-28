2d8674d9107a0a66cccc76f394e2dfd5ae4df539
Vismay Patel <vismaypikachu@gmail.com>
2022-04-28 11:00:33 -0700

    css

--- articles/defaultArticle.php
+++ articles/defaultArticle.php
@@ -57,7 +57,7 @@
     </div>
     <br>
 
-    <div style = "width: 80%; display: flex; align-items: center; justify-content: center;" class = "article">
+    <div style = "width: 80%; display: flex; align-items: center;" class = "article">
         <div class = "articleHeader">
             <h3>IF YOU SEE THIS ARTICLE</h3>
         </div>
