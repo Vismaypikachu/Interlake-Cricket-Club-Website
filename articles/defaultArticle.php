897ca16a859aaaa89ecef1f7371ee849ac5d929f
Vismay Patel <vismaypikachu@gmail.com>
2022-04-28 10:56:53 -0700

    update

--- articles/defaultArticle.php
+++ articles/defaultArticle.php
@@ -1,39 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php
-        $host = "ibcasserver.mysql.database.azure.com";
-        $username = "ibcasvismay@ibcasserver";
-        $password = "jointechsavvyyouth1!";
-        $db_name = "cricket";
-        
-        $conn = mysqli_init();
-        mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
-
-        $sql = "SELECT * FROM article WHERE articleNumber = ". $_POST['articleNumber'] .";";
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            $heading = $row['heading'];
-            $author = $row['author'];
-            $articleDate = $row['articleDate'];
-            $imageURL = $row['imageURL'];
-            $content = $row['content'];
-            $articleNumber = $row['articleNumber'];
-        }
-        else{
-            echo "Error, please contact Vismay Patel";
-        }
-
-        $conn->close();
-    ?>
-
+    
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="defaultArticle.css" type="text/css">
-    <title>Interlake Cricket Club | Test Article</title>
+    <title>Interlake Cricket Club | Default Article</title>
     <link rel="icon" type="image/x-icon" href="/src/images/ICC LOGO.svg">
 </head>
 
@@ -85,10 +59,10 @@
 
     <div class = "article">
         <div class = "articleHeader">
-            <h3><?php echo $heading; ?></h3>
+            <h3>IF YOU SEE THIS ARTICLE</h3>
         </div>
         <div class = "articleContent">
-            <p><?php echo $content; ?></p>
+            <p>Something has gone wrong</p>
         </div>
     </div>  
     <br><br>
