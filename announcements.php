7770cf98f59529fd9fbcdb626c9f9a104964707e
Vismay Patel <vismaypikachu@gmail.com>
2022-04-28 12:15:03 -0700

    css

--- announcements.php
+++ announcements.php
@@ -80,7 +80,7 @@
                                 <h5 style = \"font-family: Avenir;\" class=\"articleHeading\">" . $row['heading'] . "</h5>
                                 <p style = \"font-family: Avenir;\" class=\"articleSummary\">" . $row['summary'] . "<br>ID (internal use): ". $row["id"] ."</p>
                                 <form action=\"" . $row['articleLink'] ."\" method=\"post\">
-                                    <button class=\"btn btn-outline-primary\" style=\"margin-left: 50%;\" name = \"articleNumber\" type=\"submit\" value = \"". $row['id'] ."\">Read this</button>
+                                    <button class=\"btn btn-outline-primary\" style=\"margin-left: 25%;\" name = \"articleNumber\" type=\"submit\" value = \"". $row['id'] ."\">Read this</button>
                                 </form>
                             </div>
                         </div>
